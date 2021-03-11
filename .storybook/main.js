60f1510e19543749c6bd18fe344bb2cee6450be8
Ashish Rana <dev.livelifelively@gmail.com>
2021-03-11 18:13:40 +0530

    remove unnecessary modules

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,23 @@
+// module.exports = {
+//   stories: ['../src/**/*.stories.js'],
+//   addons: [
+//     '@storybook/addon-docs',
+//     'storybook-addon-react-docgen',
+//     '@storybook/addon-actions',
+//     {
+//       name: '@storybook/preset-ant-design',
+//       options: {
+//         lessOptions: {
+//           modifyVars: {
+//             'primary-color': '#ff0000',
+//             'border-radius-base': '2px',
+//           },
+//         },
+//       },
+//     },
+//   ],
+// };
 module.exports = {
   stories: ['../src/**/*.stories.js'],
-  addons: [
-    '@storybook/addon-docs',
-    'storybook-addon-react-docgen',
-    '@storybook/addon-actions',
-    {
-      name: '@storybook/preset-ant-design',
-      options: {
-        lessOptions: {
-          modifyVars: {
-            'primary-color': '#ff0000',
-            'border-radius-base': '2px',
-          },
-        },
-      },
-    },
-  ],
+  addons: ['@storybook/addon-docs'],
 };
